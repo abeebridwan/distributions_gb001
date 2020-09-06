d3865a9a51323b86293c743e487be9fe73c8177b
Igor Tunde <igortunde@yahoo.com>
2020-09-06 04:55:14 +0400

    setup.py added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='distributioins_gb',
       version='0.1',
       description='Gaussian and binomial distributions',
-      packages=['math_dist_ig'],
-      author="Igor Tunde",
-      author_email="igortunde@yahoo.com",
+      packages=['distributioins_gb'],
+      author="Abeeb Ridwan Olumide",
+      author_email="olumideuae@gmail.com",
       zip_safe=False)
