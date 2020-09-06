e34f3f0665f91c9beb57c8b73e7657429e460ed8
Igor Tunde <igortunde@yahoo.com>
2020-09-06 04:52:28 +0400

    setup made and inputs added to all files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributioins_gb',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['math_dist_ig'],
+      author="Igor Tunde",
+      author_email="igortunde@yahoo.com",
+      zip_safe=False)
